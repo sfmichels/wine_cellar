cf6c6d7d344023c52b2ec584dac0a863ab03bd7e
[sfmichels] <sf.michels@gmail.com>
2015-03-03 10:45:42 -0800

    Remove fields from sorting and searching.
    Meta data fields id, created_at and updated_at are removed from sorting and searching.

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,4 +1,9 @@
 class Bottle < ActiveRecord::Base
   belongs_to :wine
   accepts_nested_attributes_for :wine, :update_only => true
+
+  UNRANSACKABLE_ATTRIBUTES = %w[id created_at updated_at wine_id]
+  def self.ransackable_attributes auth_object = nil
+    (column_names - UNRANSACKABLE_ATTRIBUTES) + _ransackers.keys
+  end
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -20,5 +20,10 @@ class Wine < ActiveRecord::Base
     end
   end
 
+  UNRANSACKABLE_ATTRIBUTES = %w[id created_at updated_at]
+  def self.ransackable_attributes auth_object = nil
+    (column_names - UNRANSACKABLE_ATTRIBUTES) + _ransackers.keys
+  end
+
 end
 
